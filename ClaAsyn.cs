d78edc067a2ed3b8514a967c02c540571b60f034
jiteshwari sahu <sahujiteshwari19@gmail.com>
2018-03-17 19:42:57 +0530

    Async

---
+++ ClaAsyn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronous.cs
+{
+    class ClaAsyn
+    {
+        public static async void Test()
+        {
+            Task<int> task = Show();
+            Display();
+            int count = await task;
+            Method1(count);
+        }
+        public static async Task<int> Show()
+        {
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Test1");
+                    count += 1;
+                }
+            });
+            return count;
+        }
+        public static void Display()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Test2");
+            }
+        }
+        public static void Method1(int count)
+        {
+            Console.WriteLine("Total count is " + count);
+        }
+
+        static void Main(string[] args)
+        {
+            Test();//call
+            Console.ReadKey();
+        }
+
+    }
+
+}
+
---
+++ ClsDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronous.cs
+{
+    class ClsDemo
+    {
+        static async void CallMethod()
+        {
+            string filePath = "D:\\biz-pro\\JS.txt";
+            Task<int> task = ReadFile(filePath);
+
+            Console.WriteLine(" Other Work 1");
+            Console.WriteLine(" Other Work 2");
+            Console.WriteLine(" Other Work 3");
+
+            int length = await task;
+            Console.WriteLine(" Total length: " + length);
+
+            Console.WriteLine(" After work 1");
+            Console.WriteLine(" After work 2");
+        }
+
+        static async Task<int> ReadFile(string file)
+        {
+            int length = 0;
+
+            Console.WriteLine(" File reading is stating");
+            using (StreamReader reader = new StreamReader(file))
+            {
+                // Reads all characters from the current position to the end of the stream asynchronously  
+                // and returns them as one string.  
+                string s = await reader.ReadToEndAsync();
+
+                length = s.Length;
+            }
+            Console.WriteLine(" File reading is completed");
+            return length;
+        }
+        static void Main()
+        {
+            Task task = new Task(CallMethod);
+            task.Start();
+            task.Wait();
+            Console.Read();
+        }
+
+    }
+}
+
